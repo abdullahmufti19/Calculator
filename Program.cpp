ce4a47c17859cf390c14eca9b4491e081929d28e
abdullahmufti19 <abdullahmufti19@gmail.com>
2024-11-25 14:53:25 +0500

    Program.cpp

---
+++ Program.cpp
@@ -0,0 +1,12 @@
+// A C/C++ program to find the maximum of three numbers
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a, b, c;
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+    int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+    cout << "The maximum number is " << max << endl;
+    return 0;
+}
